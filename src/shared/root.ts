64d8f55a5886399243afbb932188cefc268413c9
AlexandrHoroshih <xad71rus@gmail.com>
2021-05-09 15:43:15 +0300

    fix: remove logger async attach to domain (this way some of the updated are skipped)

--- src/shared/root.ts
+++ src/shared/root.ts
@@ -1,9 +1,3 @@
 import { createDomain } from "effector";
 
 export const root = createDomain();
-
-if (process.env.NODE_ENV !== "production") {
-  import("effector-logger/attach").then(({ attachLogger }) => {
-    attachLogger(root);
-  });
-}
