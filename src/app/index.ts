c374a8b81ebeaf28b089e92b7906b8606421ceca
AlexandrHoroshih <xad71rus@gmail.com>
2021-05-09 12:51:40 +0300

    feat: basic (server-side) navigation

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,27 @@ import { Routes } from "pages";
 
 export const App = () => {
   h("div", () => {
+    h("nav", () => {
+      h("ul", () => {
+        h("li", () => {
+          h("a", {
+            attr: {
+              href: "/",
+            },
+            text: "Main",
+          });
+        });
+
+        h("li", () => {
+          h("a", {
+            attr: {
+              href: "/about",
+            },
+            text: "about",
+          });
+        });
+      });
+    });
     Routes();
   });
 };
