c77016ea693632e8f78588a3ba538d0be94a5a81
AlexandrHoroshih <xad71rus@gmail.com>
2021-05-09 15:44:01 +0300

    feat: use effector-history-wrap forest bindings

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,26 +1,35 @@
 import { h, spec } from "forest";
 
 import { Routes } from "pages";
+import { withPush } from "pages/history-bind";
 
 export const App = () => {
   h("div", () => {
     h("nav", () => {
       h("ul", () => {
         h("li", () => {
-          h("a", {
-            attr: {
-              href: "/",
-            },
-            text: "Main",
+          h("a", () => {
+            spec({
+              attr: {
+                href: "/",
+              },
+              text: "Main",
+            });
+
+            withPush({ params: { to: "/" } });
           });
         });
 
         h("li", () => {
-          h("a", {
-            attr: {
-              href: "/about",
-            },
-            text: "about",
+          h("a", () => {
+            spec({
+              attr: {
+                href: "/about",
+              },
+              text: "About",
+            });
+
+            withPush({ params: { to: "/about" } });
           });
         });
       });
